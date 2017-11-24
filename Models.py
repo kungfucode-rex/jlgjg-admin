9ab9812547f309917eef050e7812013b5adc7066
Nobody <nobody@yq150.onlinetools>
2017-11-24 12:28:17 +0800

    git commit by online del file 1/Models.py

--- Models.py
+++
@@ -1,9 +0,0 @@
-import time
-from ORM import Model, IntegerField, StringField, BooleanField, FloatField, TextField
-
-class User(Model):
-    __table__ = 'user'
-    id = IntegerField(primary_key=True)
-    name = StringField()
-    cnname = StringField()
-    password = StringField(ddl='varchar(20)')
